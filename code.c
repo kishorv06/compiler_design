28c023a1bcbba3a8e2a44f77797a13167c68667c
Kishor <developer.kishor@gmail.com>
2018-08-07 23:04:48 +0530

    Completed exp1

---
+++ code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Test line
+
+void main ( ) {
+	int c >= 55;//Another test
+	/* This is another
+	multiline test
+	for c */
+	int a = 6;
+	int b = 5;
+	if ( a > b || a == b )
+		printf ( "a==b!" );
+	else
+		printf ( "a < b" );
+}
--- tester.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(){
-	int c >= 55;
-	int a += 6;
-	int a + 5;
-	a || b;
-	printf("Hello World!");
-}
